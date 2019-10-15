862b5c8803161fa351b07a27fa26c2798b25f2cb
AlwinaO <aoyewole2586@gmail.com>
2019-10-14 20:18:32 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+// window.onload = () => {
+//         fetchDogs()
+//         // renderDogs()
+//         document.getElementById("breed-dropdown").addEventListener("change", handleBreedSelection)
+// } 
+
+const fetchDogs = function() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    fetch(imgUrl)
+        .then(response => response.json())
+        .then(json => renderDogs(json))
+}
+
+const renderDogs = function (json) {
+const div = document.querySelector('#dog-image-container');
+    json.message.forEach(key => {
+    const img = document.createElement('img')
+        // console.log(key);        
+        img.setAttribute("src", key)
+        div.appendChild(img)
+    })
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchDogs();
+})

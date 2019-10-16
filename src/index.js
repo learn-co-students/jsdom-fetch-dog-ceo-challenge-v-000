1487ffc44dc112dfc4b5333f23e4c798540c5e14
AlwinaO <aoyewole2586@gmail.com>
2019-10-16 12:37:30 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -29,15 +29,23 @@ const fetchBreeds = function() {
 const renderBreeds = function (json) {
     const ul = document.querySelector('#dog-breeds');
     Object.keys(json.message).forEach((element) => {
-        console.log(element);  
+        // console.log(`element is ${element}`);  
         // let index = 0;
         const li = document.createElement('li');
         const liText = document.createElement('liText');
-        console.log(liText.textContent = json.message);      
+        liText.textContent = element;
+        // console.log(liText.textContent = element);      
         // const liText = document.createElement('text').innerText;
         li.appendChild(liText);
         ul.appendChild(li);
     })
+
+    const breedColorClick = function() {
+        document.getElementsByTagName("#liText").style.color = "fuschia";
+        liText.setAttribute('onclick', element);
+        liText.addEventListener('click', breedColorClick);
+        
+    }
 }
 
 

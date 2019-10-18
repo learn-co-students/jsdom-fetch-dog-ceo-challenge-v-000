dc95d71bcd5b0cbef19cf3019078d66e01c607ac
Heather <heather.miller114@gmail.com>
2019-10-18 12:47:53 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log('%c HI', 'color: firebrick')
+
+function fetchDogs() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+        .then(response => response.json())
+        .then(json => renderDogs(json))
+}
+
+function renderDogs(json) {
+    const main = document.querySelector('#dog-image-container')
+    json.forEach(picture => {
+        
+    })
+}

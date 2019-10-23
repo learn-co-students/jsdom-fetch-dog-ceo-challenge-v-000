d4349e84f8630e8e3491eeeeec45ace54a8b5cad
Heather Miller <heather.miller114@gmail.com>
2019-10-23 11:15:35 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,25 @@ function fetchDogs() {
 
 function renderDogs(json) {
     const main = document.querySelector('#dog-image-container')
-    json.forEach(picture => {
-        
+    json.message.forEach(picture => {
+        const img = document.createElement('img')
+        img.src = picture
+        main.appendChild(img)
     })
 }
+
+function fetchBreeds() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(response => response.json())
+    .then(json => renderBreeds(json))
+}
+
+function renderBreeds(json) {
+    const ul = document.getElementById('dog-breeds')
+    let li = document.createElement('li')
+    li.innerHTML = //breed name here
+}
+
+document.addEventListener('DomContentLoaded', function() {
+    fetchDogs()
+})

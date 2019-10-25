4750b610b5fdbda1b4e9bf0f71672bca97cebdc6
Heather Miller <heather.miller114@gmail.com>
2019-10-25 12:47:21 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -18,15 +18,41 @@ function renderDogs(json) {
 function fetchBreeds() {
     fetch('https://dog.ceo/api/breeds/list/all')
     .then(response => response.json())
-    .then(json => renderBreeds(json))
+    .then(json => getBreeds(json))
 }
 
-function renderBreeds(json) {
+function getBreeds(json) {
+    let breedKeys = Object.keys(json.message)
+    breedKeys.forEach(breed => renderBreeds(breed))
+}
+
+function renderBreeds(breed) {
     const ul = document.getElementById('dog-breeds')
     let li = document.createElement('li')
-    li.innerHTML = //breed name here
+        li.addEventListener('click', function() {
+            li.style.color = "green"
+        })
+    li.innerText = breed
+    ul.appendChild(li)  
 }
 
-document.addEventListener('DomContentLoaded', function() {
-    fetchDogs()
+function filterBreeds() {
+    let input = document.getElementById('breed-dropdown').value
+    let ul = document.getElementById('dog-breeds')
+    let li = ul.getElementsByTagName('li')
+
+    for (i = 0; i < li.length; i++) {
+        let a = li[i];
+        let textValue = a.textContent || a.innerText;
+        if (textValue.indexOf(input) > -1 && textValue.indexOf(input) < 1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";  
+        }
+    }
+}
+
+document.addEventListener("DomContentLoaded", function() {
+    fetchDogs();
+    fetchBreeds();
 })

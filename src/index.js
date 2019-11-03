13712de611e22f82a3bc9622743b11ae4e636ae5
John Blank <veloblank@gmail.com>
2019-11-03 01:49:40 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,70 @@
-console.log('%c HI', 'color: firebrick')
+window.onload = function() {
+	fetchAndAddDogImages();
+	fetchBreedList();
+};
+
+function fetchAndAddDogImages() {
+	const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+	fetch(imgUrl)
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(json) {
+			let dogUrls = json["message"];
+			for (let dogUrl of dogUrls) {
+				let dogDiv = document.getElementById("dog-image-container");
+				let dogImgElement = document.createElement("img");
+				dogImgElement.src = dogUrl;
+				dogImgElement.style.width = "200px";
+				dogImgElement.style.height = "auto"; //keeps aspect ratio, but still not perfect visually
+				dogDiv.append(dogImgElement);
+			}
+		});
+}
+
+function fetchBreedList(breedFilter = null) {
+	const breedUrl = "https://dog.ceo/api/breeds/list/all";
+	fetch(breedUrl)
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(json) {
+			let breeds = Object.keys(json["message"]);
+
+			if (breedFilter) {
+				let filteredBreeds = breeds.filter(
+					breed => breed[0] === breedFilter
+				);
+				document.getElementById("dog-breeds").innerHTML = "";
+				fetchBreeds(filteredBreeds);
+			} else {
+				fetchBreeds(breeds);
+			}
+		});
+}
+
+function fetchBreeds(breeds) {
+	for (let breed of breeds) {
+		let breedLi = document.createElement("li");
+		breedLi.innerText = breed;
+		document.getElementById("dog-breeds").appendChild(breedLi);
+		breedLi.onclick = function() {
+			this.style.color = getRandomColor();
+		};
+	}
+}
+
+function getRandomColor() {
+	var letters = "0123456789ABCDEF";
+	var color = "#";
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+
+function filterDogs() {
+	let selection = document.getElementById("breed-dropdown").value;
+	fetchBreedList(selection);
+}

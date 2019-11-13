6cc6c3c5f5914ce6e18baf775eae35d4b0da710e
Daniel Goldberg <danpaulgo@aol.com>
2019-11-13 12:54:16 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
 console.log('%c HI', 'color: firebrick')
+
+function filterBreeds(letter){
+	fetch(breedUrl).then(function(response){
+		return response.json()
+	}).then(function(json){
+		for(const key in json.message){
+			if (key[0] === letter){
+				createBreedList(key)
+			}
+		}
+	})
+}
+
+function createBreedList(key){
+	let listItem = document.createElement("li")
+	listItem.innerText = key
+	listItem.addEventListener("click", function(){
+		listItem.style.color = "red"
+	})
+	document.getElementById("dog-breeds").appendChild(listItem)
+}
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+fetch(imgUrl).then(function(response){
+	return response.json()
+	}).then(function(json){
+	for(const url of json.message){
+		imgContainer = document.createElement("img")
+		imgContainer.src = url
+		imgContainer.style.display = "block"
+		document.getElementById("dog-image-container").appendChild(imgContainer)
+	}
+	
+})
+
+fetch(breedUrl).then(function(response){
+	return response.json()
+}).then(function(json){
+	for(const key in json.message){
+		createBreedList(key)
+	}
+})
+
+const letterSelector = document.getElementById("breed-dropdown")
+
+letterSelector.addEventListener("change", function(){
+	document.getElementById("dog-breeds").innerHTML = ""
+	filterBreeds(letterSelector.value)
+})
+

8970c8a62d568f40573911746d9256182ef2cd04
Shanise Barona <shanisebarona@gmail.com>
2019-11-24 20:32:42 -0800

    ✅challenge #1 done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener( "DOMContentLoaded", function () {
+  fetchImages()
+} );
+
+function fetchImages() {
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(json) {
+    debugger
+  for (const image in json.message) {
+    const img = document.createElement('img')
+      img.src = json.message[image]
+      document.getElementById('dog-image-container').appendChild(img)
+    }
+  })
+}

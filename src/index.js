8456f4957f7eb4948737d4ba1c3e38986ee1024b
Shanise Barona <shanisebarona@gmail.com>
2019-11-28 13:05:45 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 document.addEventListener( "DOMContentLoaded", function () {
   fetchImages()
+  fetchBreeds()
 } );
 
 function fetchImages() {
-  fetch("https://dog.ceo/api/breeds/image/random/4")
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl)
   .then(function(response) {
     return response.json()
   })
   .then(function(json) {
-    debugger
-  for (const image in json.message) {
-    const img = document.createElement('img')
-      img.src = json.message[image]
-      document.getElementById('dog-image-container').appendChild(img)
+    for (const image in json.message) {
+      const img = document.createElement('img')
+        img.src = json.message[image]
+        document.getElementById('dog-image-container').appendChild(img)
+      }
+  })
+}
+
+function fetchBreeds() {
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  fetch(breedUrl)
+  .then(response => response.json())
+  .then(json => {
+    breeds = Object.keys(json.message)
+    for (const breed in breeds) {
+      let li = document.createElement('li')
+      li.innerText = breeds[breed]
+      document.getElementById('dog-breeds').appendChild(li)
     }
   })
 }
+

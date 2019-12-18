e8a0d4f6f9216d91dea54d9f8c1e993b1e0d1e24
daniellecroxton <danikrist@gmail.com>
2019-12-17 21:49:19 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+
 console.log('%c HI', 'color: firebrick')
+
+function loadImages() {
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    // add image elements to the DOM FOR EACH image in the array //
+    json.forEach(addImages);
+  });
+}
+
+function addImages(image, index) {
+    document.getElementByTagName("img").appendChild(image);
+}
+
+}

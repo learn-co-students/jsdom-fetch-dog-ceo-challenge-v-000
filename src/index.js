54b348c9c6e8ba5b6e7aaa8fc88dbaa1dfedafc0
ElvinGarcia <dimix181@gmail.com>
2019-12-31 14:22:00 -0500

    done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ console.log('%c HI', 'color: firebrick');
 const imgsUrl = "https://dog.ceo/api/breeds/image/random/4";
 const breedUrl = 'https://dog.ceo/api/breeds/list/all';
 
-
+// utility function for random color generator
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 // start of dog's img and breed markup
 function validateResponse(response) {
@@ -24,14 +32,8 @@ function logError(error) {
     console.log('un error occured: \n', error );
 }
 
-
 function imgArrayMessage(arr){
     if (Array.isArray(arr)){ arr.forEach( img => { imgMarkUp(img) } ); } 
-}
-
-function breedArrayMessage(arr) {
-     arr = Object.keys(arr);
-    if (Array.isArray(arr)) { arr.forEach(breed => { breedMarkUp(breed) }); }
 }
 
 function imgMarkUp(resource){
@@ -55,8 +57,13 @@ function imgMarkUp(resource){
     li.appendChild(img);          
 }
 
+function breedArrayMessage(arr) {
+     arr = Object.keys(arr);
+    if (Array.isArray(arr)) { arr.forEach(breed => { breedMarkUp(breed) }); }
+}
 
 
+// element creator for breed and eventListener attachment
 function breedMarkUp(resource) {
     let container = document.getElementById("dog-breeds");
      if (container){
@@ -65,51 +72,72 @@ function breedMarkUp(resource) {
          li.setAttribute("class","breed-classification")
          li.innerText = resource;
          container.appendChild(li);
-         li.addEventListener("click", (self) => { colorText(self)});
+         li.addEventListener("click", (self) => { colorText(self) });
      }   
 }
 
-// end of dog's img and breed markup
-function fetchImgs(imgResource) {
-    fetch(imgResource)
-    .then(validateResponse)
-    .then(responseAsJson)
-    .then(jsonResponseMessage)
-    .then(imgArrayMessage)
-    .catch(logError);
-}
-
 // changes the elements color on click
- function colorText(element){
-     element.target.style.color = getRandomColor();
+function colorText(element){
+    element.target.style.color = getRandomColor();
 }
 
-// random color generator
-function getRandomColor() {
-    let letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+// dropdown code 
+  function dropDown() {
+     const dropdown = document.getElementById("breed-dropdown");
+     if (typeof dropdown === "object") {
+        for(const element of dropdown.children) 
+            element.addEventListener("click", () => { hideElements() })
+     }
+ }
+
+// end of dog's img and breed markup
+
+// logic to hide iteams when using dropdown
+function hideElements() {
+    const list = document.getElementById("dog-breeds");
+    const ListItems = list.getElementsByTagName("li");
+    if (event.type === "click") {
+        let targetValue = event.target.value;
+        for(let item of ListItems){
+            if (item.textContent.charAt(0) !== targetValue && item.textContent !== "---sort by:---") {    
+                item.style.display = "none";
+            }else{
+                
+                console.log("foo");
+               item.style.display = "";
+            }
+        }
+        
     }
-    return color;
 }
 
-function fetchBreed(breedUrl){
+
+
+/*
+start of code initializers
+*/
+            
+function fetchImgs(imgResource) {
+                fetch(imgResource)
+                .then(validateResponse)
+                .then(responseAsJson)
+                .then(jsonResponseMessage)
+                .then(imgArrayMessage)
+                .catch(logError);
+            }
+
+
+function breedConstruct(breedUrl){
     fetch(breedUrl)
     .then(validateResponse)
     .then(responseAsJson)
     .then(jsonResponseMessage)
     .then(breedArrayMessage)
+    .then(dropDown)
     .catch(logError)
 }
 
+
 // event loader
-window.onload = ( fetchImgs(imgsUrl), fetchBreed(breedUrl) )
+window.onload = (fetchImgs(imgsUrl), breedConstruct(breedUrl) )
 
-// Challenge 3
-// Once all of the breeds are rendered in the < ul >, 
-// add JavaScript so that the font color of a particular < li > changes on click.
-// This can be a color of your choosing.
-
-// When the user clicks any of the dog breed list items,
-// the color the text should change.

fb35954e035ccb1426f00655de069d564142d853
Arvida Jimenez <arvida.jimenez@gmail.com>
2020-01-06 21:35:56 +0000

    add functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('%c HI', 'color: firebrick')
+
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+function onPageLoad() {
+  window.addEventListener('DOMContentLoaded', (event) => {
+    fetch(imgUrl).then(function(response) {
+      return response.json();
+    }).then(addImageElements(json))
+  });
+}
+function addImageElements(json) {
+  const imagesDiv = document.getElementById("dog-image-container");
+  for (let jsonLink in json) {
+    let newImageElem = document.createElement('IMG');
+    newImageElem.src = jsonLink;
+    imagesDiv.appendChild(newImageElem);
+  }
+}

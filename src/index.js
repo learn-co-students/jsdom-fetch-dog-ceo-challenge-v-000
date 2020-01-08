5b4df8540abb903d740995df815e5125c800ff9e
daniellecroxton <danikrist@gmail.com>
2020-01-08 11:08:17 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ function loadImages() {
         .then(function(response) {
             return response.json();
         }).then(function(json) {
-            // add image elements to the DOM FOR EACH image in the array //
             json.message.forEach(image => addImages(image));
         });
 }
@@ -24,6 +23,7 @@ function addImages(image, index) {
 }
 
 
+// STEP 2
 
 // const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
@@ -37,16 +37,33 @@ function loadBreeds() {
         .then(function(response) {
             return response.json();
         }).then(function(json) {
-            json.message.forEach(breed => addBreeds(breed));
+            breed = Object.keys(json.message);
+            breed.forEach(addBreeds(breed));
         });
 }
 
-function addBreeds(breed, index) {
+function addBreeds(breeds, index) {
     breeds = document.querySelector('#dog-breeds');
     newBreed = document.createElement("li");
     newBreed.innerText = breed;
     breeds.appendChild(newBreed);
 }
+
+// STEP 3
+
+// Once all of the breeds are rendered in the <ul>, add JavaScript so that the font color of a particular <li> changes on click. This can be a color of your choosing.
+
+// When the user clicks any of the dog breed list items, the color the text should change.
+
+
+
+// STEP 4 
+
+// Once we are able to load all of the dog breeds onto the page, add JavaScript so that the user can filter breeds that start with a particular letter using a dropdown.
+
+// For example, if the user selects 'a' in the dropdown, only show the breeds with names that start with the letter a. For simplicity, the dropdown only includes the letters a-d. However, we can imagine expanding this to include the entire alphabet
+
+
 
 
 // let breeds = [];

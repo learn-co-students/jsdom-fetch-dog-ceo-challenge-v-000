cc89655e826536ae80f010e48a001eadd45bd0ad
ajimenez-ias <ajimenez@integralads.com>
2020-01-08 16:20:39 -0500

    list breeds and add eventlistener to change li color

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
+
+
 function getDogImages() {
   return fetch(imgUrl).then(resp => resp.json()).then(json => addImageElements(json));
 }
@@ -11,7 +13,6 @@ function getDogImages() {
 function addImageElements(json) {
   const imagesDiv = document.getElementById("dog-image-container");
   json.message.forEach(image => {
-    console.log(image);
     const newImageElem = document.createElement('img');
     newImageElem.src = image;
     imagesDiv.appendChild(newImageElem);
@@ -25,10 +26,25 @@ function getDogBreeds() {
 }
 
 function listBreeds(json) {
-  const dropdown = document.getElementById("breed-dropdown")
-  dropdown.forEach()
-  console.log(json)
+  const dogBreedList = document.getElementById("dog-breeds");
+  breeds = Object.keys(json.message)
+  breeds.forEach(breed => {
+    const newLiElem = document.createElement('li');
+    newLiElem.textContent = breed;
+    dogBreedList.appendChild(newLiElem);
+
+  })
+  // const allLiElements = document.getElementsByTagName('li');
+  dogBreedList.addEventListener('click', function (event) {
+    for (const liElem of dogBreedList.childNodes) {
+      if (event.target.innerText === liElem.innerText) {
+        liElem.style.color = "blue"
+      }
+    }
+  })
 }
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
   getDogImages()

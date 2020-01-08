8df78019cf2a974819c06befff4a75dd51bf9be6
ajimenez-ias <ajimenez@integralads.com>
2020-01-08 18:04:13 -0500

    implement filtering

--- src/index.js
+++ src/index.js
@@ -4,34 +4,39 @@ console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
+let allBreeds = []
 
+let filteredBreeds = []
 
 function getDogImages() {
   return fetch(imgUrl).then(resp => resp.json()).then(json => addImageElements(json));
 }
 
 function addImageElements(json) {
-  const imagesDiv = document.getElementById("dog-image-container");
+  let imagesDiv = document.getElementById("dog-image-container");
   json.message.forEach(image => {
-    const newImageElem = document.createElement('img');
+    let newImageElem = document.createElement('img');
     newImageElem.src = image;
     imagesDiv.appendChild(newImageElem);
   });   
 }
 
 function getDogBreeds() {
-  return fetch(breedUrl).then(resp => resp.json()).then(json => listBreeds(json));
+  return fetch(breedUrl).then(resp => resp.json()).then(json => createBreedsArray(json));
 }
 
-function listBreeds(json) {
+function createBreedsArray(json) {
+  allBreeds = Object.keys(json.message)
+  listBreeds(allBreeds);
+}
+
+function listBreeds(breeds) {
   let dogBreedList = document.getElementById("dog-breeds");
-  breeds = Object.keys(json.message)
   breeds.forEach(breed => {
     let newLiElem = document.createElement('li');
     newLiElem.textContent = breed;
     newLiElem.addEventListener('click', changeColor)
     dogBreedList.appendChild(newLiElem);
-
   })
 }
 
@@ -43,10 +48,15 @@ function dropdownFiltering() {
   const dropdownElem = document.getElementById('breed-dropdown');
   const dogBreedList = document.getElementById("dog-breeds");
   dropdownElem.addEventListener('change', function (){
-    dogBreedList.children.
-    console.log(this.value)
-
+    while (dogBreedList.firstChild) dogBreedList.removeChild(dogBreedList.firstChild);
+    filterBreeds(this.value);
   })
+}
+
+function filterBreeds(letter) {
+  filteredBreeds = [];
+  filteredBreeds = allBreeds.filter(breed => breed[0] === letter);
+  listBreeds(filteredBreeds);
 }
 
 

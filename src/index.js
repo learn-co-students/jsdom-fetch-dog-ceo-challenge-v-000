8b2bad275771abb80ca62cb37bf3a2bfb8442c9b
sjwakeman <swakeman@ca.rr.com>
2020-04-02 14:33:22 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ let breeds = []
 let dogBreed = []
 let test = []
 let letter = []
+let result = []
 
 //toggling color black to red and red to black
 const colorObject = {
@@ -23,178 +24,8 @@ window.onload = () => {
 // document.adddEventListener('click', callbackFunction)
 // listening for click on drop dowm a - d.
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("breed-dropdown").addEventListener('click', filterDogBreeds) 
+    document.getElementById("breed-dropdown").addEventListener('change', removeBreeds) 
 })
-
-function filterDogBreeds(event) {
-    // console.log(event.target.value)
-    // sets letter to the value of the breed=dropdown "a -d "
-    letter = (event.target.value)
-    let filtered = fetchBreed()
-    // const startsWithN = countries.filter((country) => country.startsWith("N"));
-            //  let startsWith(event) = breeds.filter(breed) => breed.startsWith(letter);
-            //  let startsWith("letter") = breeds.filter((breed) => breed.startsWith("letter"));
-        //    let list = filterBreeds().filter((breed) => breed.startsWith(letter));
-           
-//         console.log(startsWith("letter"));
-    
-             console.log(test);
-              
-             let result = test.filter((event) => event.startsWith(letter))
-             console.log(result)
-             return result
-            //  test.appendChild(result)
-};          
-
-function removeBreeds() {
-    // let originalList = document.getElementById("dog-breeds")
-    // let modifiedList = originalList.removeChild("dog-breeds")
-    //     return modifiedList
-
-    let element = document.getElementById("top");
-        while (element.firstChild) {
-            element.removeChild(element.firstChild);
-        }
-}
-
-function updateList() {
-    // fetchBreed()
-    filterBreeds()
-    let updatedList = test 
-    return updatedList //returns test 
-
-    const breedList = document.querySelector(`#breed-dropdown`)
-    breedList.appendChild(updatedList)(`result`)
-
-        // filterDogBreeds()
-        // let = updatedList = result
-    
-}
-
-// function createMenuItem(name) {
-//     let li = document.createElement('li');
-//     li.textContent = name;
-//     return li;
-// }
-// get the ul#menu
-// const menu = document.querySelector('#menu');
-
-// add menu item
-// menu.appendChild(createMenuItem('Home'));
-// menu.appendChild(createMenuItem('Services'));
-// menu.appendChild(createMenuItem('About Us'));
-
-
-
-
-
-
-
-
-    //  console.log("breeds", breeds)
-        //console.log("letter", letter) //displays letter a-d selected
-    //  breeds.filter((breed) => breed.startsWith("letter"));
-        //  console.log(startsWith("letter"));
-    // let startsWith(letter) = breeds.filter((breed) => breed.startsWith(letter));
-    //         console.log(startsWith("letter"));
-
-        // WRONG LANGUAGE TRANSLATE TO JAVASCRIPT!
-        // $('.myFilters li').on("click", function() {
-        // ('breeds').on("click", function() {
-        // var letter = $(this).text()[0];
-        //     //   $('.myFilterItems li').each(function() {
-        //     $('.breedsItems').each(function() {
-        //         if ($(this).text()[0] == letter) {
-        //            $(this).show();
-        //                 } else {
-        //                    $(this).hide();
-        //                 }
-                      
-        //         });
-        //     );
-
-         // ('.breeds li').on("click", function() {
-                    //     var letter = $(this).text()[0];
-                    //     $('.breedsItems li').each(function() {
-                    //       if ($(this).text()[0] == letter) {
-                    //         $(this).show();
-                    //       } else {
-                    //         $(this).hide();
-                    //       }
-                      
-                    //     });
-                    //   });
-
-                    //   for (i = 0; i < breeds.length; i++) {
-                    //     a = breeds[i].getElementById("a")["0"];
-                    //     txtValue = a.textContent || a.innerText 
-                //         if (txtValue.toUpperCase().indexOf(filter) >-1) {
-                //             breeds[i].style.display = ""; 
-                //         // elsif
-
-                //         // elsif
-
-                //         // elsif
-                //         } else {
-                //             breeds[i].style.display = "none";
-                //         }
-                //  }
-
-// function filterBreeds() {
-//         // const countries = ['Norway', 'Sweden',  'Denmark', 'New Zealand'];
-//         const breeds = breedUrl()
-//         dogBreed = document.getElementById("dog-breeds")
-    
-//         // const startsWithN = countries.filter((country) => country.startsWith("N"));
-//         let startsWith("letter") = breeds.filter((breed) => breed.startsWith("letter"));
-//         console.log(startsWith("letter"));
-// }
-
-// function filterBreeds() {
-//     const breeds = breedUrl()
-//     dogBreed = document.getElementById("dog-breeds")
-//     // Set empty <ul> dog-breeds
-//     dogFilter = document.getElementById("")
-    
-//     // Iterate over list using for each
-//     dogBreed.forEach(function (item, index) {
-//         console.log(item, index);
-//     })
-
-//     // Filter breeds that start with letter in <li>
-
-// }
-
-   // ('.breeds li').on("click", function() {
-                    //     var letter = $(this).text()[0];
-                    //     $('.breedsItems li').each(function() {
-                    //       if ($(this).text()[0] == letter) {
-                    //         $(this).show();
-                    //       } else {
-                    //         $(this).hide();
-                    //       }
-                      
-                    //     });
-                    //   });
-
-                                        //   for (i = 0; i < breeds.length; i++) {
-                    //     a = breeds[i].getElementById("a")["0"];
-                    //     txtValue = a.textContent || a.innerText 
-                //         if (txtValue.toUpperCase().indexOf(filter) >-1) {
-                //             breeds[i].style.display = ""; 
-                //         // elsif
-
-                //         // elsif
-
-                //         // elsif
-                //         } else {
-                //             breeds[i].style.display = "none";
-                //         }
-                //  }
-
-
-        
-    
 
 function fetchImage() {
     const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
@@ -210,7 +41,6 @@ function fetchImage() {
             } 
         });
 }
-
 
 function fetchBreed() {
     const breedUrl = 'https://dog.ceo/api/breeds/list/all'
@@ -232,6 +62,68 @@ function fetchBreed() {
              })
         });
 }
+
+function removeBreeds() {
+    let filter = filterDogBreeds
+    console.log(event)
+    let dogBreed = document.getElementById("dog-breeds")
+    // let element = document.getElementById("breed-dropdown");
+        while (dogBreed.firstChild) {
+        dogBreed.removeChild(dogBreed.firstChild);
+        }
+        filterDogBreeds(event)
+}
+
+function filterDogBreeds(event) {
+    // sets letter to the value of the breed=dropdown "a -d "
+    letter = (event.target.value)
+             console.log(test);
+             let result = test.filter((event) => event.startsWith(letter))
+             console.log(result)
+             return result
+            //  test.appendChild(result)
+            updateList(result)
+};          
+
+function updateList(result) {
+    // fetchBreed()
+    // filterBreeds()
+    let updatedList = result
+    
+    // const breedList = document.querySelector("dog-breeds")
+    const breedList = document.getElementById("dog-breeds")
+    breedList.appendChild(updatedList)
+    // return breedList.appendChild(updatedList)
+    // return updatedList
+    // return result
+    // return breedList
+    
+
+    
+
+ 
+}
+
+// function createMenuItem(name) {
+//     let li = document.createElement('li');
+//     li.textContent = name;
+//     return li;
+// }
+// get the ul#menu
+// const menu = document.querySelector('#menu');
+
+// add menu item
+// menu.appendChild(createMenuItem('Home'));
+// menu.appendChild(createMenuItem('Services'));
+// menu.appendChild(createMenuItem('About Us'));
+
+
+
+
+    
+    
+
+
 
 function filterBreeds() {
     dogBreed.innerHTML = ""

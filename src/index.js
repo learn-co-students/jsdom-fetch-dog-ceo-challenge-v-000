0bac88cf5794d269521d6da1ac52ea98d733b6b5
sdcro <sdcrouse91@gmail.com>
2020-04-28 15:16:54 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -12,14 +12,13 @@ function addDogImages (json) {
     const dogImage = document.createElement('img');
 
     dogImage.src = imgURL;
+    dogImage.alt = `Dog ${json.message.indexOf(imgURL) + 1}`; // That, or change the for loop to use a counter.
     imgContainer.appendChild(dogImage);
   }
 }
 
-function addBreedList (json) {
-  // Each key has a value of an array, some of which contain sub-breeds.
-  // I think I should try to get an output like this:
-  /*
+function createBreedList (breeds, dogBreedList) {
+  /* Example output:
     <ul>
       <li>pitbull</li>
       <li>
@@ -32,101 +31,76 @@ function addBreedList (json) {
     </ul>
   */
 
-  const breeds = json.message;
-  const dogBreedList = document.getElementById('dog-breeds');
-  const breedDropDown = document.getElementById('breed-dropdown');
+  /* Old code for future reference:
+    if (subBreeds.length > 0) { 
+      // I originally put the subBreeds in the same list as the regular breeds, instead of in a sublist.
+      // This made the list look messy and unorganized.
+      for (const subBreed of subBreeds) { // Make subBreed li's unless there are no subBreeds
+        const subBreedLi = document.createElement('li');
+        subBreedLi.textContent = `${subBreed} ${breedKey}`;
+        dogBreedList.appendChild(subBreedLi);
+      }
+    } else {
+      const breed = document.createElement('li');
+      breed.textContent = breedKey;
+      dogBreedList.appendChild(breed);
+    }
+  */
 
   for (const breedName in breeds) { 
-    const breed = document.createElement('li');
+    const breedLi = document.createElement('li');
     const subBreeds = breeds[breedName];
     
-    breed.textContent = breedName;
+    breedLi.textContent = breedName;
     
     if (subBreeds.length > 0) { // Add the sub-breeds (if any) to each breed.
       const subBreedList = document.createElement('ul');
   
       for (const subBreedName of subBreeds) {
         const subBreedLi = document.createElement('li');
-
+  
         subBreedLi.textContent = `${subBreedName} ${breedName}`;
         subBreedList.appendChild(subBreedLi);
-
+  
         subBreedLi.addEventListener('click', function () {
           changeTextColor(subBreedLi);
         }, false);
       }
   
-      breed.appendChild(subBreedList);
+      breedLi.appendChild(subBreedList);
     }
-
-    breed.addEventListener('click', function () {
-      changeTextColor(breed);
+  
+    breedLi.addEventListener('click', function () {
+      changeTextColor(breedLi);
     }, false);
-
-    dogBreedList.appendChild(breed);
+  
+    dogBreedList.appendChild(breedLi);
   }
+}
+
+function addBreedList (json) {
+  const breeds = json.message;
+  const dogBreedList = document.getElementById('dog-breeds');
+  const breedDropDown = document.getElementById('breed-dropdown');
+
+  createBreedList(breeds, dogBreedList);
 
   breedDropDown.addEventListener("change", function () {
-    console.log("Testing!"); // This part DOES get hit, so the event listener itself works.
-
-    // I think I need to use that for loop below, but break it out into a helper method.
-    // If the breed starts with breedDropDown.value, make an <li> with it and add it to the <ul>.
-    // Do the same with subBreeds - limit them by letter.
-    // There should be a String method that gets the first letter. -> startsWith()
-    // filterBreedsByLetter(breedDropDown.value);
+    // It's better do to this here (I think) than from outside of this function; all the needed constants are here.
     const firstLetter = breedDropDown.value;
-    console.log(firstLetter); // firstLetter is defined correctly.
+    const sameFirstLetterBreeds = {};
 
-    // I must have tried to do too much all at once; there's another bug in here.
-    // I probably need to break this functionality into a helper method, one that has null as a default value for firstLetter.
-    // Something like function (breeds, firstLetter = null) and whatever other arguments are needed.
-    // Maybe I need to make a duplicate of the 'breeds' constant and make the list from THAT.
-
+    // If the user selects A, e.g., only display dog breeds starting with A.
     for (const breedName in breeds) {
       if (breedName.startsWith(firstLetter)) {
-        const breed = document.createElement('li');
-        const subBreeds = breeds[breedName];
-
-        breed.textContent = breedName;
-
-        if (subBreeds.length > 0) { // Add the sub-breeds (if any) to each breed.
-          const subBreedList = document.createElement('ul');
-      
-          for (const subBreedName of subBreeds) {
-            const subBreedLi = document.createElement('li');
-    
-            subBreedLi.textContent = `${subBreedName} ${breedName}`;
-            subBreedList.appendChild(subBreedLi);
-    
-            subBreedLi.addEventListener('click', function () {
-              changeTextColor(subBreedLi);
-            }, false);
-          }
-      
-          breed.appendChild(subBreedList);
-        }
-    
-        breed.addEventListener('click', function () {
-          changeTextColor(breed);
-        }, false);
-    
-        dogBreedList.appendChild(breed);
+        sameFirstLetterBreeds[breedName] = breeds[breedName];
       }
     }
-  });
-  // If the user selects A, e.g., only display dog breeds starting with A.
+    
+    dogBreedList.textContent = ''; // This right here solved a bug; I needed to first make the dogBreedList blank.
 
-  // if (subBreeds.length > 0) { // Update: This didn't look good.
-  //   for (const subBreed of subBreeds) { // Make subBreed li's unless there are no subBreeds
-  //     const subBreedLi = document.createElement('li');
-  //     subBreedLi.textContent = `${subBreed} ${breedKey}`;
-  //     dogBreedList.appendChild(subBreedLi);
-  //   }
-  // } else {
-  //   const breed = document.createElement('li');
-  //   breed.textContent = breedKey;
-  //   dogBreedList.appendChild(breed);
-  // }
+    createBreedList(sameFirstLetterBreeds, dogBreedList);
+  }, false);
 }
 
 function changeTextColor (breed) {
@@ -141,22 +115,6 @@ function changeTextColor (breed) {
     breed.style.color = 'black';
   }
 }
-
-// function filterBreedsByLetter(letter) {
-//   // Get the container of the dog breeds
-//   const dogBreeds = document.getElementById('dog-breeds');
-
-//   // Get all of the dog breeds
-//   const eachBreed = dogBreeds.querySelectorAll('li');
-
-//   // Display only the dog breeds that start with 'letter'
-//   // Somehow, I need to replace dogBreeds' HTML without losing the rest of the HTML.
-//   // The user might select A, then B. I want to display the dog breeds that start with A,
-//   // But not lose the ones that start with B.
-
-//   // I need a copy of all of the dog breeds somewhere that I can refer to - but NOT alter - here.
-//   // I may not need to get the elements here, just limit them by first letter.
-// }
 
 document.addEventListener('DOMContentLoaded', function () {
   fetch(imgUrl).then(response => response.json()).then(addDogImages);

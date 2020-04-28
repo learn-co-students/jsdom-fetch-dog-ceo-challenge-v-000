5365c0418773dfd54c3578d8ee60a352cc38c05a
sdcro <sdcrouse91@gmail.com>
2020-04-28 01:09:45 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -36,15 +36,51 @@ function addBreedList (json) {
   const dogBreedList = document.getElementById('dog-breeds');
   const breedDropDown = document.getElementById('breed-dropdown');
 
+  for (const breedName in breeds) { 
+    const breed = document.createElement('li');
+    const subBreeds = breeds[breedName];
+    
+    breed.textContent = breedName;
+    
+    if (subBreeds.length > 0) { // Add the sub-breeds (if any) to each breed.
+      const subBreedList = document.createElement('ul');
+  
+      for (const subBreedName of subBreeds) {
+        const subBreedLi = document.createElement('li');
+
+        subBreedLi.textContent = `${subBreedName} ${breedName}`;
+        subBreedList.appendChild(subBreedLi);
+
+        subBreedLi.addEventListener('click', function () {
+          changeTextColor(subBreedLi);
+        }, false);
+      }
+  
+      breed.appendChild(subBreedList);
+    }
+
+    breed.addEventListener('click', function () {
+      changeTextColor(breed);
+    }, false);
+
+    dogBreedList.appendChild(breed);
+  }
+
   breedDropDown.addEventListener("change", function () {
+    console.log("Testing!"); // This part DOES get hit, so the event listener itself works.
+
     // I think I need to use that for loop below, but break it out into a helper method.
     // If the breed starts with breedDropDown.value, make an <li> with it and add it to the <ul>.
     // Do the same with subBreeds - limit them by letter.
     // There should be a String method that gets the first letter. -> startsWith()
     // filterBreedsByLetter(breedDropDown.value);
     const firstLetter = breedDropDown.value;
+    console.log(firstLetter); // firstLetter is defined correctly.
 
     // I must have tried to do too much all at once; there's another bug in here.
+    // I probably need to break this functionality into a helper method, one that has null as a default value for firstLetter.
+    // Something like function (breeds, firstLetter = null) and whatever other arguments are needed.
+    // Maybe I need to make a duplicate of the 'breeds' constant and make the list from THAT.
 
     for (const breedName in breeds) {
       if (breedName.startsWith(firstLetter)) {
@@ -79,36 +115,6 @@ function addBreedList (json) {
     }
   });
   // If the user selects A, e.g., only display dog breeds starting with A.
-
-  for (const breedName in breeds) { 
-    const breed = document.createElement('li');
-    const subBreeds = breeds[breedName];
-    
-    breed.textContent = breedName;
-    
-    if (subBreeds.length > 0) { // Add the sub-breeds (if any) to each breed.
-      const subBreedList = document.createElement('ul');
-  
-      for (const subBreedName of subBreeds) {
-        const subBreedLi = document.createElement('li');
-
-        subBreedLi.textContent = `${subBreedName} ${breedName}`;
-        subBreedList.appendChild(subBreedLi);
-
-        subBreedLi.addEventListener('click', function () {
-          changeTextColor(subBreedLi);
-        }, false);
-      }
-  
-      breed.appendChild(subBreedList);
-    }
-
-    breed.addEventListener('click', function () {
-      changeTextColor(breed);
-    }, false);
-
-    dogBreedList.appendChild(breed);
-  }
 
   // if (subBreeds.length > 0) { // Update: This didn't look good.
   //   for (const subBreed of subBreeds) { // Make subBreed li's unless there are no subBreeds

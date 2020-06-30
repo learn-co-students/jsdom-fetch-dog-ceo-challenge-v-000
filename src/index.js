35467f7f5fb2058b9fbe05765e2850fc71404221
Jelson Rodriguez <maskdbandit@gmail.com>
2020-06-30 17:37:44 -0400

    finalized the 4th challenge by enabling filtering mechanism. JS promise currently duplicating breeds

--- src/index.js
+++ src/index.js
@@ -1,38 +1,43 @@
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+var filterOption = ''
 
 document.addEventListener("DOMContentLoaded", () => {
-    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-    
     loadImages(imgUrl)
     loadDogBreeds(breedUrl)
 })
 
 function loadImages(url) {
-  return fetch(url)
-          .then(resp => resp.json())
-          .then(json => renderImages(json.message))
+    return fetch(url)
+            .then(resp => resp.json())
+            .then(json => renderImages(json.message))
 }
 
 function renderImages(json) {
-  const container = document.getElementById('dog-image-container')
+    const container = document.getElementById('dog-image-container')
   
-  json.forEach(imgUrl => {
-    const img = document.createElement('img')
-    img.src = `${imgUrl}`
-    container.appendChild(img)
-  })
+    json.forEach(imgUrl => {
+        const img = document.createElement('img')
+              img.src = `${imgUrl}`
+        container.appendChild(img)
+    })
 }
 
 function loadDogBreeds(url) {
     return fetch(url)
-          .then(resp => resp.json())
-          .then(json => renderDogBreeds(json.message))
+            .then(resp => resp.json())
+            .then((json) => {
+              renderDogBreeds(json.message)
+              addDropdownListener()
+            })
 }
 
 function renderDogBreeds(json) {
     const container = document.getElementById('dog-breeds')
-    const breeds = Object.keys(json)
     const dogs = json
+    var breeds = Object.keys(json)
+    // if no filter option is in place, assign the breeds otherwise assign filtered breeds
+    breeds = filterOption == '' ? breeds : breeds.filter(breed => breed[0] == filterOption)
 
     breeds.forEach(breed => {
         const li = document.createElement('li')
@@ -40,11 +45,14 @@ function renderDogBreeds(json) {
                   event.preventDefault()
                   changeTextColor(event)
               })
-        const hasMultipleBreeds = dogs[breed].length >= 1 ? true : false
+
+        const breedVariations = dogs[breed]
+        const hasMultipleBreedVariations = breedVariations.length >= 1 ? true : false
         
-        if (hasMultipleBreeds) {
-            for(let i = 0; i < dogs[breed].length; i++) {
-                li.innerHTML = `${breed} ${dogs[breed][i]}`
+        if (hasMultipleBreedVariations) {
+            for(let i = 0; i < breedVariations.length; i++) {
+                const li = document.createElement('li')
+                      li.innerHTML = `${breed} ${breedVariations[i]}`
                 container.appendChild(li)
             }
         } else {
@@ -56,4 +64,20 @@ function renderDogBreeds(json) {
 
 function changeTextColor(element) {
     element.target.style.color = 'red';
+}
+
+function addDropdownListener() {
+    document.getElementById('breed-dropdown').addEventListener("change", (event) => {
+        event.preventDefault()
+        let selectedValue = event.target.value
+        filter(selectedValue)
+    })
+}
+
+function filter(option) {
+    let container = document.getElementById("dog-breeds")
+        container.querySelectorAll('*').forEach(child => child.remove());
+    console.log('clearing listed dogs in dom')
+    filterOption = option
+    loadDogBreeds(breedUrl)
 }

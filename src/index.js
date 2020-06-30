49a163c486830aa0f5ef575e46321c9be9084016
Jelson Rodriguez <maskdbandit@gmail.com>
2020-06-29 20:44:05 -0400

    added dogs and images to dom, up to challenge 3 - font change on click

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
-console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", () => {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    
+    loadImages(imgUrl)
+    loadDogBreeds(breedUrl)
+})
+
+function loadImages(url) {
+  return fetch(url)
+          .then(resp => resp.json())
+          .then(json => renderImages(json.message))
+}
+
+function renderImages(json) {
+  const container = document.getElementById('dog-image-container')
+  
+  json.forEach(imgUrl => {
+    const img = document.createElement('img')
+    img.src = `${imgUrl}`
+    container.appendChild(img)
+  })
+}
+
+function loadDogBreeds(url) {
+    return fetch(url)
+          .then(resp => resp.json())
+          .then(json => renderDogBreeds(json.message))
+}
+
+function renderDogBreeds(json) {
+    const container = document.getElementById('dog-breeds')
+    const breeds = Object.keys(json)
+    const dogs = json
+
+    breeds.forEach(breed => {
+        const li = document.createElement('li')
+              li.addEventListener('onclick', (event) => {
+                  event.preventDefault()
+                  changeColor()
+              })
+        const hasMultipleBreeds = dogs[breed].length >= 1 ? true : false
+        
+        debugger
+        if (hasMultipleBreeds) {
+            for(let i = 0; i < dogs[breed].length; i++) {
+                debugger
+                li.innerHTML = `${breed} ${dogs[breed][i]}`
+                container.appendChild(li)
+            }
+        } else {
+            debugger
+            li.innerHTML = `${breed}`
+            container.appendChild(li)
+        }
+    })
+}
+
+function changeColor() {
+    
+}

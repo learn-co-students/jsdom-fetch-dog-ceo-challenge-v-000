1af81f7b4efa95ad8e2261c0a93475aa4f307c12
Eric Johnson <ericjohnson0216@icloud.com>
2020-09-03 19:54:17 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@
 document.addEventListener("DOMContentLoaded", function () {
     // console.log("The DOM has loaded");
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = "https://dog.ceo/api/breeds/list/all"
 
-fetch(imgUrl)
-    .then(resp => resp.json())
-    .then(json => { 
-        for (let i = 0; i < json.message.length; i++) {
-            console.log(i)
-            document.getElementById("dog-image-container").innerHTML += `<img src="${json.message[i]}"/>`;
-        }
-    });
+    fetch(imgUrl)
+        .then(resp => resp.json())
+        .then(json => { 
+            for (let i = 0; i < json.message.length; i++) {
+                // console.log(i)
+                document.getElementById("dog-image-container").innerHTML += `<img src="${json.message[i]}"/>`;
+            }
+        });
+
+    fetch(breedUrl)
+        .then(resp => resp.json())
+        .then(json => {
+            // for (let i = 0; i < json.message.length; i++) {
+            //     console.log(i)
+                document.getElementById("dog-breeds").innerHTML += `<img src="${json.message}"/>`;
+            
+        });
 });

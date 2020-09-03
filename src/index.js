34c4f562338ebad37e1f0b0fce135452610f4a1f
Eric Johnson <ericjohnson0216@icloud.com>
2020-09-03 19:34:28 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", function () {
+    // console.log("The DOM has loaded");
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => { 
+        for (let i = 0; i < json.message.length; i++) {
+            console.log(i)
+            document.getElementById("dog-image-container").innerHTML += `<img src="${json.message[i]}"/>`;
+        }
+    });
+});
